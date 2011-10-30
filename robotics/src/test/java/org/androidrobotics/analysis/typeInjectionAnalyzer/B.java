352910dd7352ecd796ace804d16ecb34b91c3043
John Ericksen <johncarl81@gmail.com>
2011-10-29 21:26:44 -0600

    moved test target classes to static inner (nested) classes

--- robotics/src/test/java/org/androidrobotics/analysis/typeInjectionAnalyzer/B.java
+++
@@ -1,8 +0,0 @@
-package org.androidrobotics.analysis.typeInjectionAnalyzer;
-
-import javax.inject.Inject;
-
-public class B {
-    @Inject
-    private C c;
-}
--- robotics/src/test/java/org/androidrobotics/analysis/typeInjectionAnalyzer/C.java
+++
@@ -1,8 +0,0 @@
-package org.androidrobotics.analysis.typeInjectionAnalyzer;
-
-import javax.inject.Inject;
-
-public class C {
-    @Inject
-    private D d;
-}
