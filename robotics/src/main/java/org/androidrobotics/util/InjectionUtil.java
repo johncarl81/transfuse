6cc5ddf79f8fdc5177e9b75afb8233421f669326
John Ericksen <johncarl81@gmail.com>
2011-11-11 18:11:11 -0700

    moved field injection code to static utility class

---
+++ robotics/src/main/java/org/androidrobotics/util/InjectionUtil.java
@@ -0,0 +1,12 @@
+package org.androidrobotics.util;
+
+/**
+ * @author John Ericksen
+ */
+public class InjectionUtil {
+
+    public static void setField(Object target, String field, Object source){
+        
+    }
+
+}
