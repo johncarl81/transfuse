a8a9eae9b6418dd739ad807063b599a21e2294e7
John Ericksen <johncarl81@gmail.com>
2012-06-30 13:20:04 -0600

    Split apart scope implementation in preparation of implementing context scope

---
+++ transfuse-api/src/main/java/org/androidtransfuse/scope/ConcurrentDoubleLockingScope.java
@@ -0,0 +1,26 @@
+package org.androidtransfuse.scope;
+
+import javax.inject.Provider;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author John Ericksen
+ */
+public class ConcurrentDoubleLockingScope implements Scope {
+
+    private ConcurrentHashMap<Class, Object> singletonMap = new ConcurrentHashMap<Class, Object>();
+
+    @Override
+    public <T> T getScopedObject(Class<T> clazz, Provider<T> provider) {
+        Object result = singletonMap.get(clazz);
+        if (result == null) {
+            Object value = provider.get();
+            result = singletonMap.putIfAbsent(clazz, value);
+            if (result == null) {
+                result = value;
+            }
+        }
+
+        return (T) result;
+    }
+}
--- transfuse-api/src/main/java/org/androidtransfuse/scope/SingletonScope.java
+++ transfuse-api/src/main/java/org/androidtransfuse/scope/SingletonScope.java
@@ -1,38 +1,19 @@
 package org.androidtransfuse.scope;
 
-import javax.inject.Provider;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * @author John Ericksen
  */
-public final class SingletonScope implements Scope {
+public final class SingletonScope {
 
     public static final String GET_INSTANCE = "getInstance";
 
-    private ConcurrentHashMap<Class, Object> singletonMap = new ConcurrentHashMap<Class, Object>();
-
-    private static final SingletonScope INSTANCE = new SingletonScope();
+    private static final Scope INSTANCE = new ConcurrentDoubleLockingScope();
 
     private SingletonScope() {
         //private singleton constructor
     }
 
-    public static SingletonScope getInstance() {
+    public static Scope getInstance() {
         return INSTANCE;
-    }
-
-    @Override
-    public <T> T getScopedObject(Class<T> clazz, Provider<T> provider) {
-        Object result = singletonMap.get(clazz);
-        if (result == null) {
-            Object value = provider.get();
-            result = singletonMap.putIfAbsent(clazz, value);
-            if (result == null) {
-                result = value;
-            }
-        }
-
-        return (T) result;
     }
 }
