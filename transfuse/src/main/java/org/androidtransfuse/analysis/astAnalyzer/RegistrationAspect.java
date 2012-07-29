13959e4d5f5bbf717ff7026e89f3d6bad53b9a02
John Ericksen <johncarl81@gmail.com>
2012-07-29 14:42:48 -0600

    Removed old action registration

--- transfuse/src/main/java/org/androidtransfuse/analysis/astAnalyzer/RegistrationAspect.java
+++ transfuse/src/main/java/org/androidtransfuse/analysis/astAnalyzer/RegistrationAspect.java
@@ -1,14 +1,9 @@
 package org.androidtransfuse.analysis.astAnalyzer;
 
-import org.androidtransfuse.analysis.adapter.ASTField;
-import org.androidtransfuse.analysis.adapter.ASTMethod;
-import org.androidtransfuse.analysis.adapter.ASTType;
 import org.androidtransfuse.gen.componentBuilder.RegistrationGenerator;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Aspect to associate an element (method, field or type) with a listener registration.  This triggers the code generator to
@@ -22,39 +17,11 @@ public class RegistrationAspect {
 
     private List<RegistrationGenerator> registrationBuilders = new ArrayList<RegistrationGenerator>();
 
-    private Set<ActionRegistration<ASTMethod>> methodRegistrations = new HashSet<ActionRegistration<ASTMethod>>();
-    private Set<ActionRegistration<ASTField>> fieldRegistrations = new HashSet<ActionRegistration<ASTField>>();
-    private Set<ActionRegistration<ASTType>> typeRegistrations = new HashSet<ActionRegistration<ASTType>>();
-
     public void addRegistrationbuilders(List<RegistrationGenerator> builders){
         registrationBuilders.addAll(builders);
     }
 
     public List<RegistrationGenerator> getRegistrationBuilders() {
         return registrationBuilders;
-    }
-
-    public void addMethodRegistration(ActionRegistration<ASTMethod> registration) {
-        methodRegistrations.add(registration);
-    }
-
-    public void addFieldRegistration(ActionRegistration<ASTField> registration) {
-        fieldRegistrations.add(registration);
-    }
-
-    public void addTypeRegistration(ActionRegistration<ASTType> registration) {
-        typeRegistrations.add(registration);
-    }
-
-    public Set<ActionRegistration<ASTMethod>> getMethodRegistrations() {
-        return methodRegistrations;
-    }
-
-    public Set<ActionRegistration<ASTField>> getFieldRegistrations() {
-        return fieldRegistrations;
-    }
-
-    public Set<ActionRegistration<ASTType>> getTypeRegistrations() {
-        return typeRegistrations;
     }
 }
