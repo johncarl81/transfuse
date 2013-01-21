1f302c1cee1b787f073f6b3a5094c3e048c535bb
John Ericksen <johncarl81@gmail.com>
2013-01-20 23:34:54 -0700

    Tweaks to javadoc update script

--- javadocs/updateJavadocs.sh
+++ javadocs/updateJavadocs.sh
@@ -1,21 +1,32 @@
-
+#!/bin/bash
+#Grab transfuse
 git clone git@github.com:johncarl81/transfuse.git
+
+# Remove old javadocs
 git rm -r api
 git rm -r main
 
-mkdir api
-mkdir main
+# Grab transfuse
+git clone git@github.com:johncarl81/transfuse.git
 
+# Build selected modules
 cd transfuse/transfuse/
 mvn clean install
 cd ../transfuse-api
 mvn clean install
 cd ../..;
 
+# Add structure folders
+mkdir api
+mkdir main
+
+# Move javadocs into place
 cp -r transfuse/transfuse/target/apidocs/* main/
 cp -r transfuse/transfuse-api/target/apidocs/* api/
 
+# Re-add javadocs
 git add main
 git add api
 
+# Cleanup
 rm -fr transfuse
