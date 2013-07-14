5eee4e5ea681e39f0a7629624fc8a2761deb0cfb
John Ericksen <johncarl81@gmail.com>
2013-07-14 11:40:28 -0600

    tweaking build script

--- build.sh
+++ build.sh
@@ -2,18 +2,23 @@
 
 if [ "$POST_BUILD" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] then
 
-	git clone -b gh-pages https://${GH_TOKEN}@github.com/johncarl81/transfuse.git site;
+	echo -e "Cloning gh-pages\n"
+	git clone -b gh-pages https://${GH_TOKEN}@github.com/johncarl81/transfuse.git site
 
-	jekyll build -d site;
+	echo -e "Building Jekyll site\n"
+	jekyll build -d site
 
-	cd site;
+	cd site
 
-	touch .nojekyll;
-	rm build.sh;
+	touch .nojekyll
+	rm build.sh
 
-	git commit -a -m "Travis build $TRAVIS_BUILD_NUMBER";
-	git push;
 
-	rm -rf site;
+	echo -e "Committing site\n"
+	git commit -a -m "Travis build $TRAVIS_BUILD_NUMBER"
+	git push
+
+	rm -rf site
 
 fi
+
