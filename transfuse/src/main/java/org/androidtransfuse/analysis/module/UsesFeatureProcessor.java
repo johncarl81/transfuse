8eeeec26edef518d47798071a15ba566fbec088d
John Ericksen <johncarl81@gmail.com>
2013-10-18 12:39:17 -0600

    Updated license

--- transfuse/src/main/java/org/androidtransfuse/analysis/module/UsesFeatureProcessor.java
+++ transfuse/src/main/java/org/androidtransfuse/analysis/module/UsesFeatureProcessor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Gustavo Matias
+ * Copyright 2013 John Ericksen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
