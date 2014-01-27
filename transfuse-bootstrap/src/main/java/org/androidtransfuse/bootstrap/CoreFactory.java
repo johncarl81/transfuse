3e3f6a77e46c75481a1778a5c0342e4132dfb04f
John Ericksen <johncarl81@gmail.com>
2014-01-27 16:15:05 -0700

    Added SynchronizedElements to bootstrap to avoid rampent NPEs

--- transfuse-bootstrap/src/main/java/org/androidtransfuse/bootstrap/CoreFactory.java
+++ transfuse-bootstrap/src/main/java/org/androidtransfuse/bootstrap/CoreFactory.java
@@ -30,6 +30,7 @@ import org.androidtransfuse.analysis.astAnalyzer.ScopeAnalysis;
 import org.androidtransfuse.analysis.module.*;
 import org.androidtransfuse.analysis.repository.AnalysisRepository;
 import org.androidtransfuse.analysis.repository.InjectionNodeBuilderRepository;
+import org.androidtransfuse.config.SynchronizedElements;
 import org.androidtransfuse.gen.*;
 import org.androidtransfuse.gen.componentBuilder.InjectionNodeImplFactory;
 import org.androidtransfuse.gen.componentBuilder.MirroredMethodGeneratorFactory;
@@ -83,7 +84,7 @@ public class CoreFactory {
     private BootstrapsInjectorGenerator bootstrapsInjectorGenerator = null;
 
     public CoreFactory(Elements elements, Messager messager, Filer filer, String namespace) {
-        this.elements = elements;
+        this.elements = new SynchronizedElements(elements);
         this.filer = filer;
         this.classNamer = new ClassNamer(namespace);
         this.validator = new Validator(messager);
